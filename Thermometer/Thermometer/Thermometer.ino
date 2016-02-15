fabcb1805f9d3ee36c4aae9b2e37dfecd6dee97e
Candelario Daniel Eguia <candelario.eguia@gmail.com>
2016-02-14 21:14:11 -0800

    Reorderd fuction to achive build

--- Thermometer/Thermometer/Thermometer.ino
+++ Thermometer/Thermometer/Thermometer.ino
@@ -14,22 +14,21 @@
  Author:	Douglas
 */
 
-/* Created by Visual Micro -->
-the setup function runs once when you press reset or power the board
-void setup() {
-
-}
-
- the loop function runs over and over again until power down or reset
-void loop() {
-  
-}
-*/
-
 //TMP36 Pin Variables
 int temperaturePin = 0; //the analog pin the TMP36's Vout (sense) pin is connected to
 						//the resolution is 10 mV / degree centigrade 
 						//(500 mV offset) to make negative temperatures an option
+						/*
+						* getVoltage() - returns the voltage on the analog input defined by
+						* pin
+						*/
+
+
+
+float getVoltage(int pin) {
+	return (analogRead(pin) * .004882814); //converting from a 0 to 1023 digital range
+										   // to 0 to 5 volts (each 1 reading equals ~ 5 millivolts
+}
 
 						/*
 						* setup() - this function runs once when you turn your Arduino on
@@ -45,17 +44,11 @@ void setup()
 void loop()                     // run over and over again
 {
 	float temperature = getVoltage(temperaturePin);  //getting the voltage reading from the temperature sensor
-	temperature = (((temperature - .5) * 100) * 1.80) + 32;          //converting from 10 mv per degree wit 500 mV offset
+	temperature = (((temperature - .5) * 100) * 1.8) + 32;          //converting from 10 mv per degree wit 500 mV offset
 													 //to degrees ((volatge - 500mV) times 100)
+	Serial.print("F: ");
 	Serial.println(temperature);                     //printing the result
+	
 	delay(1000);                                     //waiting a second
 }
 
-/*
-* getVoltage() - returns the voltage on the analog input defined by
-* pin
-*/
-float getVoltage(int pin) {
-	return (analogRead(pin) * .004882814); //converting from a 0 to 1023 digital range
-										   // to 0 to 5 volts (each 1 reading equals ~ 5 millivolts
-}
